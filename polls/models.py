34f66e310f10b58b1b397bc77a6c224409f0a04a
khanbaba <youneskhanbaba@gmail.com>
2016-03-31 17:15:48 +0430

    change from git gui

--- polls/models.py
+++ polls/models.py
@@ -4,7 +4,7 @@ from django.db import models
 from django.utils import timezone
 
 class Question (models.Model):
-	question_text = models.CharField(max_length = 200)
+	question_text = models.CharField(max_length = 201)
 	pub_date = models.DateTimeField('date published')
 	
 	def __str__(self):
